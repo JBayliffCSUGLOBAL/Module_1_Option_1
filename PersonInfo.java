d207bd3d3bcfbcdeb9e534e21cfca8d85497c644
Jonathon_Bayliff <Jonathon.Bayliff@csuglobal.edu>
2024-04-21 19:33:53 -0600

    Add files via upload

---
+++ PersonInfo.java
@@ -0,0 +1,17 @@
+public class PersonInfo {
+    public static void main(String[] args) {
+        // Declare variables
+        String firstName = "Smarty";
+        String lastName = "Pants";
+        String streetAddress = "715 Fortnite Drive";
+        String city = "Legolas";
+        String zipCode = "24689";
+
+        // Print information
+        System.out.println("First Name: " + firstName);
+        System.out.println("Last Name: " + lastName);
+        System.out.println("Street Address: " + streetAddress);
+        System.out.println("City: " + city);
+        System.out.println("Zip Code: " + zipCode);
+    }
+}
